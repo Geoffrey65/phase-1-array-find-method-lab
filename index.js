0dda19f95acfe74341a327415cdd38b636183a7f
Geoffrey <geoffrey>
2023-12-19 08:50:56 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function superbowlWin(record) {
+    // **Search for the first year the Denver Broncos won the Super Bowl.**
+    // **If no win is found, return undefined.**
+  
+    // Use `find` to locate the first object in the record with a "W" result.
+    const winningYear = record.find(game => game.result === "W");
+  
+    // If a winning year was found, extract and return it.
+    if (winningYear) {
+      return winningYear.year;
+    }
+  
+    // No win found, return undefined.
+    return undefined;
+  }
+  
